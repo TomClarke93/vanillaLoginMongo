0d15008c37f5ca8209fbc1617e13878d39a435c7
Tom Clarke <thomas.clarke09@gmail.com>
2021-05-04 15:16:53 +0100

    views directory set up. boilerplate set up

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/login', (req, res) => {
-    res.send('Login Page')
+    res.render('login');
 })
+
 app.get('/register', (req, res) => {
-    res.send('Register Page')
+    res.render('register');
 })
 app.get('/users', (req, res) => {
-    res.send('Users Page')
+    res.render('users/index');
 })
 
 app.listen(3000, () => {
